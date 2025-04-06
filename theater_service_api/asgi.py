a1f2f58d0325cec9565926c81df75ee661fd4150
Yura Chornyi <chorny.yura@gmail.com>
2025-04-06 18:28:24 +0300

    chore: rename theater_service to theater_service_api

--- theater_service_api/asgi.py
+++ theater_service_api/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for theater_service project.
+ASGI config for theater_service_api project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "theater_service.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "theater_service_api.settings")
 
 application = get_asgi_application()
--- theater_service_api/urls.py
+++ theater_service_api/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for theater_service project.
+URL configuration for theater_service_api project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
